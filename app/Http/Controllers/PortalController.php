0b3fc8432dc2146c7274fda3d5b88d1c64ac17cc
TIAGO <TIAGO@ASUS.local>
2020-11-26 08:57:52 +0000

    Update PortalController.php

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -13,12 +13,11 @@ class PortalController extends Controller
     }
     public function processarForm (Request $request) {
         $nome = $request->nome;
-        $autor = Autor::where('nome', 'like', '%'. $nome . '%')->paginate(4)
+        $autor = Autor::where('nome', 'like', '%'. $nome . '%')->paginate(4);
 
         return view('form-enviado',[   
             'nome' =>$nome,
             'autor'=>$autor
-
         ]);
     }
 }
